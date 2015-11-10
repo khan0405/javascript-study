aee399105e0113fa0cda3694ef3c3b1dbe7a8794
khan0405 <khan4257@gmail.com>
2015-11-10 21:27:42 +0900

    stringify 함수 만들기. 재귀 사용

---
+++ stringify.js
@@ -0,0 +1,49 @@
+﻿var toJSONString = (function(window, JSON) {
+    var jsonString = function(obj) {
+        var decorate = decorateDefault;
+        if (typeof obj == 'object') {
+            if (obj instanceof Array) {
+                decorate = decorateArray;
+            }
+            else {
+                decorate = decorateObject;
+            }
+        }
+        else {
+            decorate = decorateString;
+        }
+        return decorate(obj);
+    };
+
+    var decorateObject = function(obj) {
+        var result = "{";
+
+        var keys = Object.keys(obj);
+        keys.forEach(function (key, i) {
+            if (i > 0) result += ",";
+            result += '"' + key + '":' + jsonString(obj[key]);
+        });
+
+        return result + "}";
+    };
+
+    var decorateArray = function(obj) {
+        var result = "[";
+
+        for (var i = 0; i < obj.length; i++) {
+            if (i > 0) result += ',';
+            result += jsonString(obj[i]);
+        }
+        return result + "]";
+    };
+    var decorateString = function (obj) {
+        return '"' + obj + '"';
+    };
+    var decorateDefault = function (obj) {
+        return obj;
+    };
+
+    return function(obj) {
+        return jsonString(obj);
+    };
+})();
